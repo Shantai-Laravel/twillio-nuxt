873337155620aa00853ffddca9e64830068301a8
Tudor Iovita <iovitatudor@gmail.com>
2021-01-19 16:33:06 +0200

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="">
+        <Navbar></Navbar>
+        <div class="container">
+
+            <h4>Start new video conference:</h4>
+            <a href="/video-chat">Start</a>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
